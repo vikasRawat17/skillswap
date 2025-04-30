f4d15080d413e4b4699656149b28887bbf4837d7
dashcol <v96788271@gmail.com>
2025-04-30 14:31:55 +0530

    logo updated

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    port: process.env.PORT || 4173,
-    host: true,
-  },
 });
